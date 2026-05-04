c905050f4566476675ebbc419971fd74ae32399c
2024125087 <yodst0127@kau.kr>
2026-05-04 16:13:34 +0900

    Fix formatting and logic in main.c

--- p1/main.c
+++ p1/main.c
@@ -9,12 +9,11 @@ int main(void)
     int A, B, C;
     
     for (A = 1; A <= price / 900; A++ ) {
-        for (B = 2; B <= price / 750; B+=2) {
+        for (B = 2; B <= price / 750; B += 2) {
             for (C = 1; C <= price / 200; C++) {
-                if (900*A + 750*B + 200*C == price && C < A || C < B ) 
-                        printf("%d %d %d\n", A, B, C);
-                        found = true;
-                    }
+                if (900*A + 750*B + 200*C == price && (C < A || C < B)) {
+                    printf("%d %d %d\n", A, B, C);
+                    found = true;
                 }
             }
         }
