fba28eab985b284ff3832f6c4a2aed2a474de17e
2024125087 <yodst0127@kau.kr>
2026-05-04 16:10:38 +0900

    Refactor main.c to use boolean for found flag

--- p1/main.c
+++ p1/main.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     int price = 0;
-    int found = 0;
+    bool found = false;
     
     scanf("%d", &price);
     int A, B, C;
@@ -11,17 +11,16 @@ int main(void)
     for (A = 1; A <= price / 900; A++ ) {
         for (B = 2; B <= price / 750; B+=2) {
             for (C = 1; C <= price / 200; C++) {
-                if (900*A + 750*B + 200*C == price) {
-                    if (C < A || C < B) {
+                if (900*A + 750*B + 200*C == price && C < A || C < B ) 
                         printf("%d %d %d\n", A, B, C);
-                        found = 1;
+                        found = true;
                     }
                 }
             }
         }
     }
     
-    if (found == 0)
+    if (found == false)
         printf("none\n");
         
     return 0;
